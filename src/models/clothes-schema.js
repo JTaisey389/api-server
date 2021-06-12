b913d15bf273465ae72533d52a6362440f3c954a
Jason Taisey <jdtaisey89@icloud.com>
2021-06-12 16:28:41 -0700

    Got the server back up and running

--- src/models/clothes-schema.js
+++ src/models/clothes-schema.js
@@ -2,7 +2,6 @@
 
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 // eslint-disable-next-line new-cap
 const clothesCart = mongoose.Schema({
   name: { type: Number, required: true}, // CHECK NUMBER!!!!!!!
@@ -13,14 +12,3 @@ const clothesCart = mongoose.Schema({
 const clothingModel = mongoose.model('clothes', clothesCart);
 
 module.exports = clothingModel;
-=======
-const clothesSchema = mongoose.Schema({
-  size: { type: Number, required: true },
-  color: { type: String, required: true },
-  type: { type: String, uppercase: true, enum: ['SHIRT', 'PANTS', 'HAT'] }
-});
-
-const clothesModel = mongoose.model('food', clothesSchema);
-
-module.exports = clothesModel;
->>>>>>> 004c68e2848c4568a8288d6c3f92b966000df1b3
